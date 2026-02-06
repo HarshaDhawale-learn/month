f020555e2d859126982535d23acfff8db8aab0c9
Harsha Dhawale <harsha.dhawale8@gmail.com>
2026-02-06 22:29:12 +0530

    Initial JavaScript practice

---
+++ number.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 20;
+
+if (a > b) {
+    console.log("a is the largest number:", a);
+} else if (b > a) {
+    console.log("b is the largest number:", b);
+} else {
+    console.log("Both numbers are equal!");
+}
---
+++ year.js
@@ -0,0 +1,7 @@
+let year = 2026;
+
+if (year % 4 === 0) {
+    console.log("It is a leap year.");
+} else {
+    console.log("It is not a leap year.");
+}
